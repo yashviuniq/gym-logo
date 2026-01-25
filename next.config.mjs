d1842200b25728495f3ea26d3469dff98c65b871
rk-2005 <youremail@example.com>
2026-01-25 13:59:02 +0530

    first

--- next.config.mjs
+++ next.config.mjs
@@ -25,5 +25,5 @@ export default withPWA({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === "development" || process.env.VERCEL === "1",
 })(nextConfig);

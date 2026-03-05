61f5bbbb0b791ecc47e2763c1560dabcf5ba9b96
rk-2005 <youremail@example.com>
2026-03-05 15:17:01 +0530

    done

--- app/(admin)/members/add/page.js
+++ app/(admin)/members/add/page.js
@@ -901,7 +901,7 @@ export default function AddMemberPage() {
                     className="w-full pl-10 pr-4 py-2 bg-gray-50 border border-gray-200 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none transition-all text-sm"
                     value={formData.startDate}
                     onChange={(e) =>{ updateForm("startDate", e.target.value)
-                      updateForm("joindate",e.target.value);
+                      updateForm("joinDate",e.target.value);
                     }
                     }
                   />
